4ec00d68cacdc12ea4a771dce5d648f50609a5b8
Leonardo Cerqueira <guy.fawkes.05111605.e@gmail.com>
2019-10-26 16:24:50 -0300

    Started Critter class

---
+++ Source/Hellrain/Critter.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Critter.generated.h"
+
+UCLASS()
+class HELLRAIN_API ACritter : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACritter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(EditAnywhere)
+	class UCameraComponent* Camera;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	FVector CurrentVelocity;
+	
+	UPROPERTY(EditAnywhere)
+	float MaxSpeed;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+};
