e7e96c7bafcb274c09bd123645ab34d385b0d7e9
Leonardo Cerqueira <guy.fawkes.05111605.e@gmail.com>
2019-10-26 21:20:42 -0300

    Sphere movement implemented

--- Source/Hellrain/ColliderMovementComponent.cpp
+++ Source/Hellrain/ColliderMovementComponent.cpp
@@ -3,3 +3,27 @@
 
 #include "ColliderMovementComponent.h"
 
+void UColliderMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    // Check to make sure everything is still valid and that we are allowed to move
+    if (PawnOwner == nullptr || UpdatedComponent == nullptr || ShouldSkipUpdate(DeltaTime))
+    {
+        return;
+    }
+
+    // Get and clear the vector from Collider
+    FVector DesiredMovementThisFrame = ConsumeInputVector() * DeltaTime * 150.f;
+    if (DesiredMovementThisFrame.IsNearlyZero() == false)
+    {
+        FHitResult Hit;
+        SafeMoveUpdatedComponent(DesiredMovementThisFrame, UpdatedComponent->GetComponentRotation(), true, Hit);
+
+        // If we bump into something, slide amond the size of it
+        if (Hit.IsValidBlockingHit())
+        {
+            SlideAlongSurface(DesiredMovementThisFrame, 1.f - Hit.Time, Hit.Normal, Hit);
+        }
+    }
+}
